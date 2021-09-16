73cb2133f9022a2863c64f6b9f33dd300fae0995
cheng <jinchengjoker@foxmail.com>
2021-09-16 19:31:32 +0800

    Home 布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,11 +13,32 @@ export default {
   <main>
     <h1>Maple UI</h1>
     <div class="actions">
-      <a href>GitHub</a>
-      <a href>Start</a>
+      <a href="https://github.com/JinChengJoker">GitHub</a>
+      <router-link to="/doc">Start</router-link>
     </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
+main {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
