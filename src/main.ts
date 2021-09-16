ae6665288bc4c51a64cbcfa2bf13cdcdaa7755cd
cheng <jinchengjoker@foxmail.com>
2021-09-16 18:51:29 +0800

    use vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createWebHashHistory, createRouter } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: HelloWorld,
+    }
+  ]
+})
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
