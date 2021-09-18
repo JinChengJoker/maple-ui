9f262fb9a7c6fb5e36c82eedd7edd81a004ba185
cheng <jinchengjoker@foxmail.com>
2021-09-18 18:48:45 +0800

    button default

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,25 @@
 <template>
-  <button>
+  <button class="mp-button">
     <slot></slot>
   </button>
 </template>
+
+<style lang="scss">
+.mp-button {
+  display: inline-block;
+  padding: 12px 20px;
+  border-radius: 4px;
+  background-color: #409eff;
+  border: 1px solid #409eff;
+  color: #fff;
+  transition: all 250ms;
+
+  &:hover,
+  &:focus {
+    background-color: #66b1ff;
+    border-color: #66b1ff;
+    cursor: pointer;
+    outline: none;
+  }
+}
+</style>
