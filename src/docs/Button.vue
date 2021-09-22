2c678dd13729b98e8cd0c618e3ff821966d71f49
cheng <jinchengjoker@foxmail.com>
2021-09-22 16:21:43 +0800

    Button OK

--- src/docs/Button.vue
+++ src/docs/Button.vue
@@ -5,18 +5,64 @@ import Button from '../lib/Button.vue'
 export default defineComponent({
   components: {
     Button
-  },
-  setup() {
-    const onClick = () => {
-      console.log('hi')
-    }
-    return {
-      onClick
-    }
   }
 })
 </script>
 
 <template>
-  <Button @click="onClick">我是按钮</Button>
+  <section>
+    <h2>基础用法</h2>
+    <div class="demo">
+      <Button>默认按钮</Button>
+      <Button type="primary">主要按钮</Button>
+      <Button type="success">成功按钮</Button>
+      <Button type="warning">警告按钮</Button>
+      <Button type="danger">危险按钮</Button>
+    </div>
+  </section>
+  <section>
+    <h2>不同尺寸</h2>
+    <div class="demo">
+      <Button size="big">大号按钮</Button>
+      <Button>默认按钮</Button>
+      <Button size="small">小号按钮</Button>
+    </div>
+  </section>
+  <section>
+    <h2>禁用状态</h2>
+    <div class="demo">
+      <Button disabled>默认按钮</Button>
+      <Button type="primary" disabled>主要按钮</Button>
+      <Button type="success" disabled>成功按钮</Button>
+      <Button type="warning" disabled>警告按钮</Button>
+      <Button type="danger" disabled>危险按钮</Button>
+    </div>
+  </section>
+  <section>
+    <h2>加载中</h2>
+    <div class="demo">
+      <Button loading>默认按钮</Button>
+      <Button type="primary" loading>加载中</Button>
+    </div>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+  + section {
+    margin-top: 20px;
+  }
+
+  > .demo {
+    padding: 20px;
+    border: 1px solid #dcdfe6;
+    margin-top: 10px;
+
+    > .mp-button {
+      + .mp-button {
+        margin-left: 10px;
+      }
+    }
+  }
+}
+</style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,48 @@
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+const types = ['primary', 'success', 'warning', 'danger']
+const sizes = ['big', 'small']
+
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    size: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  setup(props) {
+    const { type, size } = props
+    const classes = {
+      [`mp-button-${type}`]: types.includes(type),
+      [`mp-button-${size}`]: sizes.includes(size),
+    }
+    return {
+      classes
+    }
+  }
+})
+</script>
+
 <template>
-  <button class="mp-button">
+  <button class="mp-button" :class="classes" :disabled="disabled || loading" :loading="loading">
+    <span class="mp-button-loading" v-if="loading"></span>
     <slot></slot>
   </button>
 </template>
@@ -7,19 +50,132 @@
 <style lang="scss">
 .mp-button {
   display: inline-block;
-  padding: 12px 20px;
+  padding: 10px 16px;
+  border: 1px solid #dcdfe6;
   border-radius: 4px;
-  background-color: #409eff;
-  border: 1px solid #409eff;
-  color: #fff;
+  color: #606266;
+  background-color: #fff;
+  font-size: 14px;
+  line-height: 1;
   transition: all 250ms;
+
+  &.mp-button-primary {
+    color: #fff;
+    border-color: #409eff;
+    background-color: #409eff;
+  }
+  &.mp-button-success {
+    color: #fff;
+    border-color: #67c23a;
+    background-color: #67c23a;
+  }
+  &.mp-button-warning {
+    color: #fff;
+    border-color: #e6a23c;
+    background-color: #e6a23c;
+  }
+  &.mp-button-danger {
+    color: #fff;
+    border-color: #f56c6c;
+    background-color: #f56c6c;
+  }
+
+  &.mp-button-big {
+    padding: 12px 18px;
+    font-size: 16px;
+  }
+  &.mp-button-small {
+    padding: 8px 14px;
+    font-size: 12px;
+  }
 
   &:hover,
   &:focus {
-    background-color: #66b1ff;
-    border-color: #66b1ff;
     cursor: pointer;
     outline: none;
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
+
+    &.mp-button-primary {
+      color: #fff;
+      border-color: #66b1ff;
+      background-color: #66b1ff;
+    }
+    &.mp-button-success {
+      color: #fff;
+      border-color: #85ce61;
+      background-color: #85ce61;
+    }
+    &.mp-button-warning {
+      color: #fff;
+      border-color: #ebb563;
+      background-color: #ebb563;
+    }
+    &.mp-button-danger {
+      color: #fff;
+      border-color: #f78989;
+      background-color: #f78989;
+    }
+  }
+
+  &[disabled] {
+    cursor: not-allowed;
+    color: #c0c4cc;
+    border-color: #e4e7ed;
+    background-color: #fff;
+
+    &.mp-button-primary {
+      color: #fff;
+      border-color: #a0cfff;
+      background-color: #a0cfff;
+    }
+    &.mp-button-success {
+      color: #fff;
+      border-color: #b3e19d;
+      background-color: #b3e19d;
+    }
+    &.mp-button-warning {
+      color: #fff;
+      border-color: #f3d19e;
+      background-color: #f3d19e;
+    }
+    &.mp-button-danger {
+      color: #fff;
+      border-color: #fab6b6;
+      background-color: #fab6b6;
+    }
+  }
+
+  > .mp-button-loading {
+    display: inline-block;
+    width: 14px;
+    height: 14px;
+    border-radius: 8px;
+    border-color: #c0c4cc #c0c4cc #c0c4cc transparent;
+    border-style: solid;
+    border-width: 2px;
+    margin-right: 4px;
+    vertical-align: bottom;
+    animation: mp-button-spin 1s infinite linear;
+  }
+
+  &.mp-button-primary,
+  &.mp-button-success,
+  &.mp-button-warning,
+  &.mp-button-danger {
+    > .mp-button-loading {
+      border-color: #fff #fff #fff transparent;
+    }
+  }
+}
+
+@keyframes mp-button-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
