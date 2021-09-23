839b9725ed8146f294a29dc1b4c7aa206aa09e21
cheng <jinchengjoker@foxmail.com>
2021-09-23 15:37:29 +0800

    create tabs and tab

--- src/docs/Tabs.vue
+++ src/docs/Tabs.vue
@@ -1,5 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+
+export default defineComponent({
+  components: {
+    Tabs,
+    Tab
+  }
+})
+</script>
+
 <template>
-  <div>
-    Tabs
-  </div>
+  <section>
+    <h2>用法</h2>
+    <div class="demo">
+      <Tabs>
+        <Tab title="标题1">内容1</Tab>
+        <Tab title="标题2">内容2</Tab>
+      </Tabs>
+    </div>
+  </section>
 </template>
